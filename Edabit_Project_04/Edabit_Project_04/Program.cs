bc0b97197831ee21b33f92760e6bc4690ec71dfe
CARICOM12 <0190a@ukr.net>
2020-05-03 15:07:36 +0300

    Add files via upload

---
+++ Edabit_Project_04/Edabit_Project_04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Edabit_Project_04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
+//Create a function that takes two numbers as arguments and return their sum.
